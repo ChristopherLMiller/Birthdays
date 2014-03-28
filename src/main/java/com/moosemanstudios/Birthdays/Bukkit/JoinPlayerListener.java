024c48912d515aca2b67afd546d7da599f04b8dd
moose51789 <moose51789@gmail.com>
2014-03-28 13:19:59 -0400

    Fix null exception

--- src/main/java/com/moosemanstudios/Birthdays/Bukkit/JoinPlayerListener.java
+++ src/main/java/com/moosemanstudios/Birthdays/Bukkit/JoinPlayerListener.java
@@ -30,24 +30,26 @@ public class JoinPlayerListener implements Listener {
 			BirthdayManager.getInstance().getConfig().set(player.getName() + ".informed", BirthdayManager.getInstance().getConfig().getInt(player.getName() + ".informed") + 1);
 			BirthdayManager.getInstance().saveConfig();
 		} else {
-			// see if its the players birthday
-			String birthday = BirthdayManager.getInstance().getConfig().getString(player.getName() + ".birthday");
-			int claimedYear = BirthdayManager.getInstance().getConfig().getInt(player.getName() + ".claimed");
+			if (BirthdayManager.getInstance().getConfig().contains(player.getName() + ".birthday")) {
+				// see if its the players birthday
+				String birthday = BirthdayManager.getInstance().getConfig().getString(player.getName() + ".birthday");
+				int claimedYear = BirthdayManager.getInstance().getConfig().getInt(player.getName() + ".claimed");
 
-			// get and format the current date to match what we store
-			String date = new SimpleDateFormat("MM/dd").format(Calendar.getInstance().getTime());
-			int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(Calendar.getInstance().getTime()));
+				// get and format the current date to match what we store
+				String date = new SimpleDateFormat("MM/dd").format(Calendar.getInstance().getTime());
+				int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(Calendar.getInstance().getTime()));
 
-			if ((claimedYear < year) && (birthday.equals(date))) {
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f*"));
-				player.sendMessage(ChatColor.AQUA + "HAPPY BIRTHDAY!");
-				player.sendMessage(ChatColor.AQUA + "To get your birthday present, type " + ChatColor.WHITE + "/birthdays claim");
+				if ((claimedYear < year) && (birthday.equals(date))) {
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f*"));
+					player.sendMessage(ChatColor.AQUA + "HAPPY BIRTHDAY!");
+					player.sendMessage(ChatColor.AQUA + "To get your birthday present, type " + ChatColor.WHITE + "/birthdays claim");
 
-				if (plugin.broadcastOnJoin) {
-					Bukkit.broadcastMessage(ChatColor.AQUA + "Today is " + player.getName() + "'s Birthday!");
+					if (plugin.broadcastOnJoin) {
+						Bukkit.broadcastMessage(ChatColor.AQUA + "Today is " + player.getName() + "'s Birthday!");
+					}
+
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f*"));
 				}
-
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f* &9* &a* &b* &c* &d* &e* &f*"));
 			}
 		}
     }
